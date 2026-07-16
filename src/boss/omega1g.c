2dcb0d8f6676015e8a6ad8c10e34507dd373d867
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-16 18:57:12 -0400

    Match OmegaGold_Update and the mode handlers
    Splits omega1g.inc around the matched functions: OmegaGold_Update,
    OmegaGold_Die, OmegaGold_Disappear, goldOmega1_0805b420,
    makeGoldOmega1Mode2, FUN_0805b4a8, goldOmega1Neutral, FUN_0805b744,
    FUN_0805b7f0 and the six mode nops. OmegaGold_Init, the death
    sequence, changeGoldOmega1Mode and the laser stay as asm.

--- src/boss/omega1g.c
+++ src/boss/omega1g.c
@@ -2,6 +2,8 @@
 #include "boss/omega1.h"
 #include "collision.h"
 #include "global.h"
+#include "overworld.h"
+#include "palette_animation.h"
 
 // Omega (1st gold)
 
@@ -34,15 +36,184 @@ struct Entity* CreateOmegaGold(Coords32* c, u8 n) {
 
 // --------------------------------------------
 
-INCASM("asm/boss/omega1g.inc");
+INCASM("asm/boss/omega1g_a.inc");
 
-void FUN_0805b41c(struct Boss* p);
-void FUN_0805b45c(struct Boss* p);
-void FUN_0805b4a4(struct Boss* p);
+static const BossFunc sUpdates1[8];
+static const BossFunc sUpdates2[8];
+static const BossFunc sDeads[2];
+static const struct Collision sCollisions[8];
+static void floatGoldOmega1(struct Omega1* p);
+
+void OmegaGold_Update(struct Boss* p) {
+  u8 m;
+  if (!((p->body).status & BODY_STATUS_DEAD)) {
+    if (*(s16*)((u8*)p + 0xa4) != 0) {
+      goto alive;
+    }
+  }
+  if (gStageRun.missionStatus & MISSION_PLAYER_DEAD) {
+    goto alive;
+  }
+  SET_BOSS_ROUTINE(p, ENTITY_DIE);
+  OmegaGold_Die(p);
+  return;
+
+alive:
+  m = (p->s).mode[1];
+  if (m > 1 && m != 6) {
+    StepPaletteAnimation(0x66);
+  }
+  (sUpdates1[(p->s).mode[1]])(p);
+  (sUpdates2[(p->s).mode[1]])(p);
+}
+
+void OmegaGold_Die(struct Boss* p) { (sDeads[(p->s).mode[1]])(p); }
+
+void OmegaGold_Disappear(struct Boss* p) {
+  RemovePaletteAnimation(0xb);
+  RemovePaletteAnimation(0x66);
+  RemovePaletteAnimation(0x67);
+  RemovePaletteAnimation(0x10f);
+  DeleteBoss((void*)p);
+}
+
+INCASM("asm/boss/omega1g_b.inc");
+
+bool8 FUN_0805b41c(struct Boss* p) { return TRUE; }
+
+void goldOmega1_0805b420(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).mode[2] = 1;
+      FALLTHROUGH;
+    case 1:
+      if ((p->s).scriptEntity->flags & 1) {
+        gOverworld.state[1] = 1;
+        (p->s).mode[1] = 1, (p->s).mode[2] = 0;
+      }
+      break;
+  }
+}
+
+bool8 FUN_0805b45c(struct Boss* p) { return TRUE; }
+
+void makeGoldOmega1Mode2(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).mode[2] = 1;
+      (p->s).work[2] = 0x3c;
+      FALLTHROUGH;
+    case 1:
+      if ((p->s).work[2] == 0 || --(p->s).work[2] == 0) {
+        (p->s).mode[2]++;
+      }
+      break;
+    case 2:
+      (p->s).mode[1] = (p->s).mode[2];
+      (p->s).mode[2] = 0;
+      break;
+  }
+}
+
+bool8 FUN_0805b4a4(struct Boss* p) { return TRUE; }
+
+void FUN_0805b4a8(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).mode[2] = 1;
+      FALLTHROUGH;
+    case 1:
+      if (!(gStageRun.vm.active & VM_ACTIVE)) {
+        (p->s).mode[1] = 3, (p->s).mode[2] = 0;
+      }
+      break;
+  }
+}
+
+INCASM("asm/boss/omega1g_c.inc");
+
+void goldOmega1Neutral(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SetDDP(&p->body, &sCollisions[1]);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).work[3] = 0x1e;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      floatGoldOmega1((struct Omega1*)p);
+      break;
+  }
+}
+
+bool8 nop_0805b5dc(struct Boss* p) { return TRUE; }
+
+INCASM("asm/boss/omega1g_d.inc");
+
+bool8 nop_0805b740(struct Boss* p) { return TRUE; }
+
+void FUN_0805b744(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).spr.xflip = 0;
+      (p->s).spr.oam.xflip = 0;
+      (p->s).flags &= ~X_FLIP;
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).work[2] = 0xff;
+      (p->s).work[3] = -1;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      if ((p->s).work[2] != 0) {
+        (p->s).work[2]--;
+      } else if ((p->s).work[3] != 0 && --(p->s).work[3] == 0) {
+        (p->s).work[2] = 0x3c;
+        (p->s).mode[2]++;
+      }
+      StepPaletteAnimation(0xb);
+      floatGoldOmega1((struct Omega1*)p);
+      break;
+    case 2:
+      if ((p->s).work[2] == 0 || --(p->s).work[2] == 0) {
+        (p->s).mode[1] = 3, (p->s).mode[2] = 0;
+      }
+      StepPaletteAnimation(0xb);
+      floatGoldOmega1((struct Omega1*)p);
+      break;
+  }
+}
+
+bool8 nop_0805b7ec(struct Boss* p) { return TRUE; }
+
+void FUN_0805b7f0(struct Boss* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).spr.xflip = 0;
+      (p->s).spr.oam.xflip = 0;
+      (p->s).flags &= ~X_FLIP;
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).work[2] = 0xFF;
+      (p->s).work[3] = -1;
+      RemovePaletteAnimation(0x66);
+      StartPaletteAnimation(0x67, 0x300);
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      if ((*(struct Entity**)((u8*)p + 0xc8))->mode[2] > 0x1d) {
+        StartPaletteAnimation(0x66, 0x300);
+        (p->s).mode[1] = 3, (p->s).mode[2] = 0;
+      } else {
+        StepPaletteAnimation(0x67);
+        floatGoldOmega1((struct Omega1*)p);
+      }
+      break;
+  }
+}
+
 void changeGoldOmega1Mode(struct Boss* p);
-void nop_0805b5dc(struct Boss* p);
-void nop_0805b740(struct Boss* p);
-void nop_0805b7ec(struct Boss* p);
 static bool32 nop_0805b874(void* _ UNUSED);
 
 // clang-format off
@@ -60,13 +231,7 @@ static const BossFunc sUpdates1[8] = {
 
 // --------------------------------------------
 
-void goldOmega1_0805b420(struct Boss* p);
-void makeGoldOmega1Mode2(struct Boss* p);
-void FUN_0805b4a8(struct Boss* p);
-void goldOmega1Neutral(struct Boss* p);
 void goldOmega1Laser(struct Boss* p);
-void FUN_0805b744(struct Boss* p);
-void FUN_0805b7f0(struct Boss* p);
 static void FUN_0805b878(struct Entity* p);
 
 // clang-format off
