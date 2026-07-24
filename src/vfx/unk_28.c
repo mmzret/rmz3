b331e528318590644003228100fb82c26710ace2
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-23 22:58:09 -0400

    Match Ghost28_Init
    Same init as VFX59_Init/BatringNecro_Init (the drifting-ghost family):
    sprite setup, xflip + drift direction from work[0], then either the
    fixed 0xFF lifetime or the 127 + (RANDOM & 7) roll from work[1].
    Compiled with the matching flags and verified byte-identical against
    the ROM function at 080B9EF4.

--- src/vfx/unk_28.c
+++ src/vfx/unk_28.c
@@ -40,125 +40,32 @@ struct Entity* FUN_080b9ebc(Coords32* c, u8 n, motion_t m, s32 val) {
   return (void*)p;
 }
 
-NAKED static void Ghost28_Init(struct VFX* p) {
-  asm(".syntax unified\n\
-	push {r4, lr}\n\
-	adds r4, r0, #0\n\
-	bl InitNonAffineMotion\n\
-	ldrb r1, [r4, #0xa]\n\
-	movs r0, #1\n\
-	movs r3, #0\n\
-	adds r2, r0, #0\n\
-	orrs r2, r1\n\
-	movs r0, #2\n\
-	orrs r2, r0\n\
-	orrs r2, r3\n\
-	strb r2, [r4, #0xa]\n\
-	ldrb r1, [r4, #0x10]\n\
-	cmp r1, #0\n\
-	bne _080B9F60\n\
-	movs r0, #0xef\n\
-	ands r2, r0\n\
-	strb r2, [r4, #0xa]\n\
-	adds r0, r4, #0\n\
-	adds r0, #0x4c\n\
-	strb r1, [r0]\n\
-	adds r2, r4, #0\n\
-	adds r2, #0x4a\n\
-	ldrb r1, [r2]\n\
-	movs r0, #0x11\n\
-	rsbs r0, r0, #0\n\
-	ands r0, r1\n\
-	strb r0, [r2]\n\
-	b _080B9F80\n\
-_080B9F60:\n\
-	movs r1, #1\n\
-	movs r0, #0x10\n\
-	orrs r2, r0\n\
-	strb r2, [r4, #0xa]\n\
-	adds r0, r4, #0\n\
-	adds r0, #0x4c\n\
-	strb r1, [r0]\n\
-	adds r3, r4, #0\n\
-	adds r3, #0x4a\n\
-	movs r2, #0x10\n\
-	ldrb r1, [r3]\n\
-	movs r0, #0x11\n\
-	rsbs r0, r0, #0\n\
-	ands r0, r1\n\
-	orrs r0, r2\n\
-	strb r0, [r3]\n\
-_080B9F80:\n\
-	ldrb r0, [r4, #0x10]\n\
-	cmp r0, #0\n\
-	bne _080B9F8C\n\
-	movs r0, #0xc0\n\
-	rsbs r0, r0, #0\n\
-	b _080B9F8E\n\
-_080B9F8C:\n\
-	movs r0, #0xc0\n\
-_080B9F8E:\n\
-	str r0, [r4, #0x5c]\n\
-	movs r3, #0\n\
-	str r3, [r4, #0x60]\n\
-	ldrb r0, [r4, #0x11]\n\
-	cmp r0, #0\n\
-	bne _080B9FB8\n\
-	movs r0, #0xff\n\
-	strb r0, [r4, #0x12]\n\
-	ldr r1, _080B9FB4 @ =gVFXFnTable\n\
-	ldrb r0, [r4, #9]\n\
-	lsls r0, r0, #2\n\
-	adds r0, r0, r1\n\
-	movs r1, #1\n\
-	str r1, [r4, #0xc]\n\
-	ldr r0, [r0]\n\
-	ldr r0, [r0, #4]\n\
-	str r0, [r4, #0x14]\n\
-	strb r1, [r4, #0xd]\n\
-	b _080B9FEA\n\
-	.align 2, 0\n\
-_080B9FB4: .4byte gVFXFnTable\n\
-_080B9FB8:\n\
-	ldr r2, _080B9FFC @ =RNG_0202f388\n\
-	ldr r1, [r2]\n\
-	ldr r0, _080BA000 @ =0x000343FD\n\
-	muls r0, r1, r0\n\
-	ldr r1, _080BA004 @ =0x00269EC3\n\
-	adds r0, r0, r1\n\
-	lsls r0, r0, #1\n\
-	lsrs r1, r0, #1\n\
-	str r1, [r2]\n\
-	lsrs r0, r0, #0x11\n\
-	movs r1, #7\n\
-	ands r0, r1\n\
-	adds r0, #0x7f\n\
-	strb r0, [r4, #0x12]\n\
-	ldr r1, _080BA008 @ =gVFXFnTable\n\
-	ldrb r0, [r4, #9]\n\
-	lsls r0, r0, #2\n\
-	adds r0, r0, r1\n\
-	movs r1, #1\n\
-	str r1, [r4, #0xc]\n\
-	ldr r0, [r0]\n\
-	ldr r0, [r0, #4]\n\
-	str r0, [r4, #0x14]\n\
-	movs r0, #2\n\
-	strb r0, [r4, #0xd]\n\
-_080B9FEA:\n\
-	strb r3, [r4, #0xe]\n\
-	strb r3, [r4, #0xf]\n\
-	adds r0, r4, #0\n\
-	bl Ghost28_Update\n\
-	pop {r4}\n\
-	pop {r0}\n\
-	bx r0\n\
-	.align 2, 0\n\
-_080B9FFC: .4byte RNG_0202f388\n\
-_080BA000: .4byte 0x000343FD\n\
-_080BA004: .4byte 0x00269EC3\n\
-_080BA008: .4byte gVFXFnTable\n\
- .syntax divided\n");
+static void Ghost28_Init(struct VFX* p) {
+  EnableSpriteAnimation_Normal(&p->s);
+  (p->s).flags |= DISPLAY;
+  (p->s).flags |= FLIPABLE;
+  if ((p->s).work[0] == 0) {
+    SET_XFLIP(p, FALSE);
+  } else {
+    SET_XFLIP(p, TRUE);
+  }
+  if ((p->s).work[0] == 0) {
+    (p->s).d.x = -PIXEL(3) / 4;
+  } else {
+    (p->s).d.x = PIXEL(3) / 4;
+  }
+  (p->s).d.y = 0;
+
+  if ((p->s).work[1] == 0) {
+    (p->s).work[2] = 0xFF;
+    SET_VFX_ROUTINE(p, ENTITY_UPDATE);
+    (p->s).mode[1] = 1, (p->s).mode[2] = 0, (p->s).mode[3] = 0;
+  } else {
+    (p->s).work[2] = 127 + (RANDOM(RNG_0202f388) & 7);
+    SET_VFX_ROUTINE(p, ENTITY_UPDATE);
+    (p->s).mode[1] = 2, (p->s).mode[2] = 0, (p->s).mode[3] = 0;
+  }
+  Ghost28_Update(p);
 }
 
 // --------------------------------------------
