2cb35058b791f49e8c709cbb50b141a254fea230
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-25 21:04:14 -0400

    Match 24 Omega gold hand functions

--- src/enemy/omega1g_hand.c
+++ src/enemy/omega1g_hand.c
@@ -2,6 +2,10 @@
 #include "collision.h"
 #include "enemy.h"
 #include "global.h"
+#include "script.h"
+
+static const EnemyFunc PTR_ARRAY_08368218[2];
+static const struct Collision sCollisions[7];
 
 void OmegaGoldHand_Init(struct Enemy* p);
 void OmegaGoldHand_Update(struct Enemy* p);
@@ -38,14 +42,268 @@ struct Entity* FUN_08082bbc(struct Entity* q, u8 kind) {
   return p;
 }
 
-INCASM("asm/enemy/omega1g_hand.inc");
+INCASM("asm/enemy/omega1g_hand_a.inc");
 
-void FUN_08083284(struct Enemy* p);
-void FUN_080833c8(struct Enemy* p);
-void FUN_0808340c(struct Enemy* p);
-void FUN_080835b4(struct Enemy* p);
-void FUN_08083640(struct Enemy* p);
-void FUN_08083e14(struct Enemy* p);
+void OmegaGoldHand_Die(struct Enemy* p) {
+  (PTR_ARRAY_08368218[(p->s).mode[1]])(p);
+}
+
+INCASM("asm/enemy/omega1g_hand_b.inc");
+
+void FUN_08083258(struct Enemy* p) {
+  if ((p->s).mode[2] == 0) {
+    (p->s).flags &= ~DISPLAY;
+    SET_ENEMY_ROUTINE(p, ENTITY_EXIT);
+  }
+}
+
+bool8 FUN_08083284(struct Enemy* p) { return TRUE; }
+
+void FUN_08083288(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).flags2 |= WHITE_PAINTABLE;
+      (p->s).invincibleID = ((p->s).unk_28)->uniqueID;
+      SetMotion(&p->s, 0x900);
+      SET_XFLIP(p, FALSE);
+      SET_YFLIP(p, FALSE);
+      (p->s).coord.y = *(s32*)((u8*)p + 0xb8) + ((p->s).unk_28)->coord.y;
+      (p->s).coord.x = *(s32*)((u8*)p + 0xb4) + ((p->s).unk_28)->coord.x;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateMotionGraphic(&p->s);
+      if (((p->s).unk_28)->scriptEntity->flags & 1) {
+        (p->s).mode[1] = 1;
+        (p->s).mode[2] = 0;
+      }
+      break;
+  }
+}
+
+void FUN_08083328(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).flags2 |= WHITE_PAINTABLE;
+      (p->s).invincibleID = ((p->s).unk_28)->uniqueID;
+      SetMotion(&p->s, MOTION(SM009_OMEGA_HAND, 1));
+      SET_XFLIP(p, FALSE);
+      SET_YFLIP(p, FALSE);
+      (p->s).coord.y = *(s32*)((u8*)p + 0xb8) + ((p->s).unk_28)->coord.y;
+      (p->s).coord.x = *(s32*)((u8*)p + 0xb4) + ((p->s).unk_28)->coord.x;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateMotionGraphic(&p->s);
+      if (((p->s).unk_28)->scriptEntity->flags & 1) {
+        (p->s).mode[1] = 1;
+        (p->s).mode[2] = 0;
+      }
+      break;
+  }
+}
+
+bool8 FUN_080833c8(struct Enemy* p) { return TRUE; }
+
+void FUN_080833cc(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).mode[2] = 1;
+      // fallthrough
+    case 1:
+      (p->s).mode[1] = 2;
+      (p->s).mode[2] = 0;
+      break;
+  }
+}
+
+void FUN_080833ec(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).mode[2] = 1;
+      // fallthrough
+    case 1:
+      (p->s).mode[1] = 2;
+      (p->s).mode[2] = 0;
+      break;
+  }
+}
+
+bool8 FUN_0808340c(struct Enemy* p) {
+  if (((struct Entity*)(p->s).unk_28)->mode[1] == 5) {
+    (p->s).mode[1] = 4;
+    (p->s).mode[2] = 0;
+  }
+  return TRUE;
+}
+
+INCASM("asm/enemy/omega1g_hand_c.inc");
+
+void FUN_080834fc(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).flags2 |= WHITE_PAINTABLE;
+      (p->s).invincibleID = ((p->s).unk_28)->uniqueID;
+      SetMotion(&p->s, 0x901);
+      SET_XFLIP(p, FALSE);
+      SET_YFLIP(p, FALSE);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      SetDDP(&p->body, &sCollisions[3]);
+      (p->s).work[2] = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1: {
+      s32 wob;
+      (p->s).work[2] += 2;
+      wob = gSineTable[(p->s).work[2]] << 1;
+      (p->s).coord.y = *(s32*)((u8*)p + 0xb8) + ((p->s).unk_28)->coord.y + wob;
+      (p->s).coord.x = *(s32*)((u8*)p + 0xb4) + ((p->s).unk_28)->coord.x;
+      UpdateMotionGraphic(&p->s);
+      break;
+    }
+  }
+}
+
+bool8 FUN_080835b4(struct Enemy* p) { return TRUE; }
+
+void FUN_080835b8(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SET_XFLIP(p, 0);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateMotionGraphic(&p->s);
+      break;
+  }
+}
+
+void FUN_080835fc(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SET_XFLIP(p, 0);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateMotionGraphic(&p->s);
+      break;
+  }
+}
+
+bool8 FUN_08083640(struct Enemy* p) { return TRUE; }
+
+INCASM("asm/enemy/omega1g_hand_d.inc");
+
+bool8 FUN_08083e14(struct Enemy* p) { return TRUE; }
+
+void FUN_08083e18(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      EXIT_BODY(p);
+      (p->s).d.x = 0;
+      (p->s).d.y = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      (p->s).d.y += 0x40;
+      if ((p->s).d.y > 0x40) {
+        (p->s).d.y = 0x40;
+      }
+      (p->s).coord.y += (p->s).d.y;
+      UpdateMotionGraphic(&p->s);
+      break;
+  }
+}
+
+void FUN_08083e68(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SET_XFLIP(p, 0);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateMotionGraphic(&p->s);
+      break;
+  }
+}
+
+void nop_08083eac(struct Enemy* p) {}
+
+bool8 FUN_08083eb0(struct Enemy* p) {
+  p->buffer[0xd] = 1;
+  (p->s).mode[1] = 4;
+  (p->s).mode[2] = 0;
+  return TRUE;
+}
+
+void FUN_08083ec4(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SetMotion(&p->s, MOTION(SM009_OMEGA_HAND, 0));
+      SET_XFLIP(p, FALSE);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).work[2] = 0;
+      p->buffer[0xd] = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1: {
+      s32 wob;
+      (p->s).work[2] += 2;
+      wob = gSineTable[(p->s).work[2]];
+      (p->s).coord.y = *(s32*)((u8*)p + 0xb8) + ((p->s).unk_28)->coord.y + wob;
+      (p->s).coord.x = *(s32*)((u8*)p + 0xb4) + ((p->s).unk_28)->coord.x;
+      UpdateMotionGraphic(&p->s);
+      break;
+    }
+  }
+}
+
+void FUN_08083f4c(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SetMotion(&p->s, MOTION(SM009_OMEGA_HAND, 1));
+      SET_XFLIP(p, FALSE);
+      SET_YFLIP(p, FALSE);
+      (p->s).d.y = 0;
+      (p->s).d.x = 0;
+      (p->s).work[2] = 0;
+      p->buffer[0xd] = 0;
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1: {
+      s32 wob;
+      (p->s).work[2] += 2;
+      wob = gSineTable[(p->s).work[2]] << 1;
+      (p->s).coord.y = *(s32*)((u8*)p + 0xb8) + ((p->s).unk_28)->coord.y + wob;
+      (p->s).coord.x = *(s32*)((u8*)p + 0xb4) + ((p->s).unk_28)->coord.x;
+      UpdateMotionGraphic(&p->s);
+      break;
+    }
+  }
+}
+
+bool8 FUN_08083ff0(struct Enemy* p) { return TRUE; }
+
+INCASM("asm/enemy/omega1g_hand_e.inc");
+
+bool8 FUN_080846fc(struct Enemy* p) { return TRUE; }
+
+void FUN_08084700(struct Enemy* p) {}
+
+bool8 FUN_08083284(struct Enemy* p);
+bool8 FUN_080833c8(struct Enemy* p);
+bool8 FUN_0808340c(struct Enemy* p);
+bool8 FUN_080835b4(struct Enemy* p);
+bool8 FUN_08083640(struct Enemy* p);
+bool8 FUN_08083e14(struct Enemy* p);
 
 // clang-format off
 static const EnemyFunc PTR_ARRAY_08368188[6] = {
@@ -58,9 +316,9 @@ static const EnemyFunc PTR_ARRAY_08368188[6] = {
 };
 // clang-format on
 
-void FUN_08083eb0(struct Enemy* p);
-void FUN_08083ff0(struct Enemy* p);
-void FUN_080846fc(struct Enemy* p);
+bool8 FUN_08083eb0(struct Enemy* p);
+bool8 FUN_08083ff0(struct Enemy* p);
+bool8 FUN_080846fc(struct Enemy* p);
 
 // clang-format off
 static const EnemyFunc PTR_ARRAY_083681a0[6] = {
