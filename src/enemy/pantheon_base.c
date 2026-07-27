4a45874e58e1926c0cef8211e634eb624c7475d1
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-26 22:14:18 -0400

    Match 2 stub(s) in pantheon_base

--- src/enemy/pantheon_base.c
+++ src/enemy/pantheon_base.c
@@ -2,7 +2,19 @@
 #include "enemy.h"
 #include "global.h"
 
-INCASM("asm/enemy/pantheon_base.inc");
+void nop_0808a140(struct Enemy* p);
+
+INCASM("asm/enemy/pantheon_base_a.inc");
+
+void nop_0808a140(struct Enemy* p) {
+}
+
+INCASM("asm/enemy/pantheon_base_b.inc");
+
+void nop_0808a3f4(struct Enemy* p) {
+}
+
+INCASM("asm/enemy/pantheon_base_c.inc");
 
 void PantheonBase_Init(struct Enemy* p);
 void PantheonBase_Update(struct Enemy* p);
