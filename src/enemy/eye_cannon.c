c6514edd6200b43b761da1ddeb8eb943f5130f25
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-27 15:19:21 -0400

    Match 6 function(s) in eye_cannon

--- src/enemy/eye_cannon.c
+++ src/enemy/eye_cannon.c
@@ -1,6 +1,19 @@
 #include "collision.h"
 #include "enemy.h"
 #include "global.h"
+#include "vfx.h"
+#include "story.h"
+#include "score.h"
+
+bool8 FUN_08084744(struct Enemy* p);
+void FUN_080847b8(struct Enemy* p);
+void FUN_080b7f70(struct Enemy* p, struct Coord* c, motion_t* m, s32 n);
+static const struct Collision sCollisions[3];
+static const EnemyFunc sDeads[1];
+static const motion_t sMotions[3];
+static const EnemyFunc sUpdates1[6];
+static const EnemyFunc sUpdates2[6];
+static const u32 u32_ARRAY_08368358[4];
 
 void EyeCannon_Init(struct Enemy* p);
 void EyeCannon_Update(struct Enemy* p);
@@ -8,12 +21,20 @@ void EyeCannon_Die(struct Enemy* p);
 
 // clang-format off
 const EnemyRoutine gEyeCannonRoutine = {
-    [ENTITY_INIT] =      EyeCannon_Init,
-    [ENTITY_UPDATE] =    EyeCannon_Update,
-    [ENTITY_DIE] =       EyeCannon_Die,
+    [ENTITY_INIT] =      (void*)EyeCannon_Init,
+    [ENTITY_UPDATE] =    (void*)EyeCannon_Update,
+    [ENTITY_DIE] =       (void*)EyeCannon_Die,
     [ENTITY_DISAPPEAR] = (void*)DeleteEnemy,
     [ENTITY_EXIT] =      (EnemyFunc)DeleteEntity,
 };
+
+
+
+
+void _killEyeCannon(struct Enemy* p);
+void FUN_08084cbc(struct Enemy* p);
+void FUN_08084974(struct Enemy* p);
+void FUN_08084934(struct Enemy* p);
 // clang-format on
 
 static void onCollision(struct Body* body UNUSED, Coords32* r1 UNUSED, Coords32* r2 UNUSED) {
@@ -31,7 +52,87 @@ static bool8 FUN_08084708(struct Enemy* p) {
   return FALSE;
 }
 
-INCASM("asm/enemy/eye_cannon.inc");
+INCASM("asm/enemy/eye_cannon_a.inc");
+
+void EyeCannon_Update(struct Enemy* p) {
+  if (!FUN_08084708(p)) {
+    FUN_080847b8(p);
+    if (!FUN_08084744(p)) {
+      (sUpdates1[(p->s).mode[1]])((void*)p);
+      (sUpdates2[(p->s).mode[1]])((void*)p);
+    }
+  }
+}
+
+void EyeCannon_Die(struct Enemy* p) {
+  (sDeads[(p->s).mode[1]])((void*)p);
+}
+
+INCASM("asm/enemy/eye_cannon_b.inc");
+
+void FUN_08084934(struct Enemy* p) {
+  struct Entity** slot = (struct Entity**)((u8*)p + 0xb4);
+  if (*slot == NULL || isKilled(*slot)) {
+    *slot = NULL;
+    SetDDP(&p->body, &sCollisions[1]);
+    if (!IsFrozen(&p->s)) {
+      (p->s).mode[1] = 1;
+      (p->s).mode[2] = 0;
+    }
+  }
+}
+
+void FUN_08084974(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SetMotion(&p->s, MOTION(0x66, 0));
+      SetDDP(&p->body, &sCollisions[2]);
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateEntityAnim(&p->s);
+      break;
+  }
+}
+
+INCASM("asm/enemy/eye_cannon_c.inc");
+
+void FUN_08084cbc(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      PlaySound(0x8f << 1);
+      SetMotion(&p->s, MOTION(0x66, 3));
+      (p->s).mode[2]++;
+      // fallthrough
+    case 1:
+      UpdateEntityAnim(&p->s);
+      if ((p->s).motion.state == 3) {
+        (p->s).mode[1] = (p->s).motion.state;
+        (p->s).mode[2] = 0;
+      }
+      break;
+  }
+}
+
+void _killEyeCannon(struct Enemy* p) {
+  struct Coord c;
+  EXIT_BODY(p);
+  (p->s).flags &= ~DISPLAY;
+  c.x = (p->s).coord.x;
+  c.y = (p->s).coord.y + 0x1200;
+  FUN_080b7f70(p, &c, (motion_t*)sMotions, 3);
+  CreateSmoke(1, &c);
+  PlaySound(0x2a);
+  TryDropItem(3, &(p->s).coord);
+  if (gScore.enemyCount <= 0x270e) {
+    gScore.enemyCount++;
+  }
+  DropEnemyDisk(p, &(p->s).coord);
+  SET_ENEMY_ROUTINE(p, ENTITY_EXIT);
+  if ((p->s).work[0] != 0) {
+    SET_FLAG(gCurStory.s.gameflags, u32_ARRAY_08368358[(p->s).work[0] - 1]);
+  }
+}
 
 void FUN_08084934(struct Enemy* p);
 void FUN_08084930(struct Enemy* p);
