f9d26427e4bc33fe828b29a2362bf44c480ff512
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-07 00:45:13 -0400

    enemy/unk_63: decompile 2 functions

--- src/enemy/unk_63.c
+++ src/enemy/unk_63.c
@@ -121,7 +121,107 @@ static void FUN_08094bf0(struct Enemy* p) {
   }
 }
 
-INCASM("asm/enemy/unk_63.inc");
+void FUN_08094c80(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).work[3] = 2;
+      (p->s).flags &= ~DISPLAY;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      (p->s).work[2]--;
+      if ((u8)(p->s).work[2] != 0) {
+        break;
+      }
+      goto inc;
+    case 2:
+      FUN_08094a54((p->s).coord.x, (p->s).coord.y, (p->s).work[0]);
+      (p->s).work[2] = 0x1E;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 3:
+      (p->s).work[2]--;
+      if ((u8)(p->s).work[2] != 0) {
+        break;
+      }
+      (p->s).work[3]--;
+      if ((u8)(p->s).work[3] != 0) {
+        goto set2;
+      }
+    inc:
+      (p->s).mode[2]++;
+      break;
+    set2:
+      (p->s).mode[2] = 2;
+      break;
+    case 4:
+      (p->s).work[2] = 0x3C;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 5: {
+      s32 w;
+      if ((p->s).work[2] % 9 == 0) {
+        FUN_080949d4((p->s).coord.x, (p->s).coord.y, (p->s).work[0]);
+      }
+      w = (p->s).work[2] - 1;
+      (p->s).work[2] = w;
+      w = (u8)w;
+      if (w == 0) {
+        (p->s).mode[2] = w;
+        (p->s).work[2] = 0x78;
+      }
+      break;
+    }
+  }
+}
+
+void FUN_08094d48(struct Enemy* p) {
+  switch ((p->s).mode[2]) {
+    case 0:
+      SetMotion(&p->s, 0xC800);
+      (p->s).unk_coord.y = 0;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1: {
+      s32 x;
+      s32 y;
+      s32 t;
+      if (*((u8*)pZero2 + 0x94) != 0) {
+        SetDDP(&p->body, &sCollisions[0]);
+      } else {
+        SetDDP(&p->body, &sCollisions[1]);
+      }
+      x = (p->s).coord.x + (p->s).d.x;
+      (p->s).coord.x = x;
+      y = (p->s).coord.y + (p->s).d.y;
+      (p->s).coord.y = y;
+      t = (p->s).unk_coord.y + (p->s).unk_coord.x;
+      (p->s).unk_coord.y = t;
+      if (t > 0x3800) {
+        register u8 f asm("r0");
+        register u8 t1 asm("r1");
+        register u8 k2 asm("r1");
+        register s32 z asm("r2");
+        FUN_08094a54(x, y, (p->s).work[1]);
+        t1 = (p->s).flags;
+        f = 0xFE;
+        f &= t1;
+        asm volatile("" ::"r"(t1));
+        z = 0;
+        k2 = 0xFD;
+        f &= k2;
+        (p->s).flags = f;
+        *(u32*)((u8*)p + 0x8c) = z;
+        *(u32*)((u8*)p + 0x90) = z;
+        *(u8*)((u8*)p + 0x94) = z;
+        (p->s).flags &= ~COLLIDABLE;
+        SET_ENEMY_ROUTINE(p, 3);
+      }
+      UpdateEntityAnim(&p->s);
+      break;
+    }
+  }
+}
 
 static void FUN_08094e0c(struct Enemy* p) {
   switch ((p->s).mode[2]) {
