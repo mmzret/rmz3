1025ece6ba1b3b84786598283eecd4408da3fa3e
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-07 00:49:00 -0400

    vfx/unk_75: decompile 2 functions

--- src/vfx/unk_75.c
+++ src/vfx/unk_75.c
@@ -2,6 +2,7 @@
 #include "global.h"
 #include "vfx.h"
 #include "vfx/unk_common.h"
+#include "stagerun.h"
 
 // Cattatank の残骸?
 
@@ -92,9 +93,224 @@ static void Ghost75_Die(struct Entity* p) {
 
 // --------------------------------------------
 
-static void nop_080c76fc(void* _) {}
+static void nop_080c76fc(void* _ UNUSED) {}
 
-INCASM("asm/vfx/unk_75.inc");
+void FUN_080c7700(struct VFX* p) {
+  s32 t = (p->s).work[2] - 1;
+  (p->s).work[2] = t;
+  t <<= 24;
+  if (t == 0) {
+    register s32 r asm("r4");
+    struct Coord c;
+    CreateSmoke(2, &(p->s).coord);
+    {
+      register u32 rv asm("r1");
+      register u32 ka asm("r0");
+      rv = RNG_0202f388;
+      ka = 0x343FD;
+      asm volatile("add %0, %1, #0" : "=&l"(r) : "l"(rv));
+      r *= ka;
+      ka = 0x269EC3;
+      r += ka;
+      r <<= 1;
+      RNG_0202f388 = (u32)r >> 1;
+      r = (u32)r >> 0x11;
+      ka = 3;
+      r &= ka;
+    }
+    c.x = (p->s).coord.x;
+    c.y = (p->s).coord.y;
+    {
+      register s32 m5 asm("r5");
+      {
+        register u8 w0 asm("r1");
+        register s32 m2 asm("r2");
+        register struct Coord* cp asm("r0");
+        w0 = (p->s).work[0];
+        m5 = 0xD50E;
+        cp = &c;
+        asm volatile("add %0, %1, #0" : "=&l"(m2) : "l"(m5));
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp, w0, m2, r);
+      }
+      {
+        register u8 w1 asm("r1");
+        register s32 m3 asm("r2");
+        register struct Coord* cp1 asm("r0");
+        w1 = (p->s).work[0];
+        m3 = 0xD50D;
+        cp1 = &c;
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp1, w1, m3, r);
+      }
+      {
+        register u8 w2 asm("r1");
+        register s32 m6 asm("r2");
+        register struct Coord* cp2 asm("r0");
+        w2 = (p->s).work[0];
+        cp2 = &c;
+        asm volatile("add %0, %1, #0" : "=&l"(m6) : "l"(m5));
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp2, w2, m6, r);
+      }
+    }
+    PlaySound(0x2A);
+    SET_VFX_ROUTINE(p, ENTITY_DIE);
+    return;
+  }
+
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).work[2] = 0x1E;
+      (p->s).d.y = -0x180;
+      SetMotion(&p->s, MOTION(0xD5, 0x06));
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      (p->s).d.y += 0x10;
+      if ((p->s).d.y > 0x700) {
+        (p->s).d.y = 0x700;
+      }
+      (p->s).coord.y += (p->s).d.y;
+      (p->s).coord.x += (p->s).d.x;
+      UpdateEntityAnim(&p->s);
+      break;
+  }
+}
+
+void FUN_080c77f0(struct VFX* p) {
+  register s32 md asm("r9");
+  {
+    s32 t = (p->s).work[2] - 1;
+    (p->s).work[2] = t;
+    if ((u8)t == 0) {
+      goto die;
+    }
+  }
+  {
+    register u32 at asm("r1");
+    at = (u16)FUN_080098a4((p->s).coord.x, (p->s).coord.y);
+    if (at == 0) {
+      goto normal;
+    }
+    {
+      register s32 k asm("r0");
+      k = 0x80 * 256;
+      k &= at;
+      if (k != 0) {
+        goto normal;
+      }
+    }
+    if ((p->s).d.y <= 0) {
+      goto normal;
+    }
+  }
+die : {
+  register struct Camera* cam asm("r0");
+  register struct Coord* cp asm("r4");
+  cam = &gStageRun.vm.camera;
+  asm("" : "+r"(cam));
+  cp = &(p->s).coord;
+  if (Camera_GetDistance(cam, cp) <= 0x80 * 64) {
+    CreateSmoke(2, cp);
+    PlaySound(0x2A);
+  }
+  SET_VFX_ROUTINE(p, ENTITY_DIE);
+  return;
+}
+normal:
+  md = (p->s).mode[2];
+  switch (md) {
+    case 0: {
+      register const s32** tb asm("r4");
+      register u16* mp asm("r8");
+      register u32 mul asm("r6");
+      register u32 add asm("r5");
+      register u32 s1 asm("r4");
+      register u32* rp2 asm("r3");
+      register const s32* q asm("r2");
+      tb = (const s32**)0x0836F920;
+      asm("" : "+r"(tb));
+      {
+        register u16* mtmp asm("r1");
+        register u32 i asm("r0");
+        asm volatile("mov %0, #0x74\n\tadd %0, %0, %1" : "=&l"(mtmp) : "l"(p));
+        mp = mtmp;
+        i = (u16)*mtmp % 3;
+        i <<= 16;
+        i >>= 14;
+        i += (u32)tb;
+        {
+          register const s32* qb asm("r0");
+          register s32 off asm("r2");
+          qb = *(const s32**)i;
+          off = *(s32*)((u8*)p + 0x78);
+          off <<= 3;
+          q = (const s32*)(off + (u32)qb);
+        }
+      }
+      {
+        register u32 r0v asm("r0");
+        register u32 r1v asm("r1");
+        register u32* rp asm("r3");
+        rp = &RNG_0202f388;
+        asm("" : "+r"(rp));
+        rp2 = rp;
+        r0v = *rp;
+        mul = 0x343FD;
+        asm volatile("add %0, %1, #0" : "=&l"(r1v) : "l"(r0v));
+        r1v *= mul;
+        add = 0x269EC3;
+        r1v += add;
+        r1v <<= 1;
+        asm("" : "+r"(r1v));
+        s1 = r1v >> 1;
+        *rp = s1;
+        r1v >>= 0x11;
+        r0v = 0x1F;
+        r1v &= r0v;
+        (p->s).d.y = q[1] + r1v;
+      }
+      {
+        register u32 r1w asm("r1");
+        register u32 r0w asm("r0");
+        asm volatile("add %0, %1, #0" : "=&l"(r1w) : "l"(s1));
+        r1w *= mul;
+        r1w += add;
+        r1w <<= 1;
+        asm("" : "+r"(r1w));
+        *rp2 = r1w >> 1;
+        r1w >>= 0x11;
+        r0w = 0x3F;
+        r1w &= r0w;
+        (p->s).d.x = q[0] - r1w;
+      }
+      ((void (*)(struct Entity*, u32))SetMotion)(&p->s, *mp);
+      {
+        register u8 fl asm("r1");
+        register s32 f asm("r0");
+        fl = (p->s).flags;
+        f = 1;
+        f |= fl;
+        (p->s).flags = f;
+      }
+      {
+        register s32 mv asm("r1");
+        mv = md;
+        (p->s).work[3] = mv;
+      }
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    }
+    case 1:
+      (p->s).work[3]++;
+      (p->s).d.y += 0x10;
+      if ((p->s).d.y > 0xE0 * 8) {
+        (p->s).d.y = 0xE0 * 8;
+      }
+      (p->s).coord.y += (p->s).d.y;
+      (p->s).coord.x += (p->s).d.x;
+      UpdateEntityAnim(&p->s);
+      break;
+  }
+}
 
 // --------------------------------------------
 
