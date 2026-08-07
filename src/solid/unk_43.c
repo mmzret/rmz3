b520d7f2288dfb25f7a45c91d60bbe7265d6ffef
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-07 01:02:03 -0400

    solid/unk_43: decompile 2 functions

--- src/solid/unk_43.c
+++ src/solid/unk_43.c
@@ -1,7 +1,11 @@
 #include "collision.h"
 #include "global.h"
+#include "vfx.h"
 #include "solid.h"
 #include "story.h"
+#include "stagerun.h"
+
+void FUN_08014b04();
 
 static const struct Collision sCollision;
 
@@ -43,7 +47,86 @@ static void Solid43_Init(struct Solid* p) {
   Solid43_Die(p);
 }
 
-INCASM("asm/solid/unk_43.inc");
+void Solid43_Update(struct Solid* p) {
+  u32 status = (p->body).status;
+  if (status & BODY_STATUS_WHITE) {
+   if (status & BODY_STATUS_RECOILED) {
+    s16 i;
+    s32 z;
+    const SolidRoutine* const* tbl;
+    (p->body).status = 0;
+    (p->body).prevStatus = 0;
+    (p->body).invincibleTime = 0;
+    {
+      u8 f = ~COLLIDABLE & (p->s).flags;
+      f |= DISPLAY;
+      f |= FLIPABLE;
+      (p->s).flags = f;
+    }
+    InitNonAffineMotion(&p->s);
+    SetMotion(&p->s, MOTION(SM156_UNK_ICE, 0));
+    FUN_08014b04();
+    AppendQuake(4, &(p->s).coord);
+    PlaySound(0x11D);
+    i = 0;
+    z = 0;
+    tbl = (const SolidRoutine* const*)&gSolidFnTable[43];
+    for (; i < 3; i++) {
+      struct Solid* q = (struct Solid*)AllocEntityLast(gSolidHeaderPtr);
+      if (q != NULL) {
+        s32 y;
+        (q->s).renderPrio = 30;
+        (q->s).id = 43;
+        (q->s).onUpdate = (void*)(**tbl)[ENTITY_INIT];
+        (q->s).tileNum = z;
+        (q->s).palID = z;
+        (q->s).flags2 |= WHITE_PAINTABLE;
+        (q->s).invincibleID = (q->s).uniqueID;
+        (q->s).work[0] = 1;
+        (q->s).coord.x = (p->s).coord.x;
+        y = (p->s).coord.y;
+        if (i == 0) {
+          y += -0x1000;
+        }
+        (q->s).coord.y = y;
+        if (i > 1) {
+          (q->s).coord.y = y + (i << 12);
+        }
+      }
+    }
+    SET_SOLID_ROUTINE(p, ENTITY_DIE);
+    Solid43_Die(p);
+   }
+  }
+}
+
+void Solid43_Die(struct Solid* p) {
+  struct Coord c;
+  if ((p->s).mode[1] == 0) {
+    register u32 t asm("r0");
+    u32 r;
+    t = RNG_0202f388 * 0x343FD + 0x269EC3;
+    t <<= 1;
+    r = t >> 1;
+    c.x = (p->s).coord.x + ((t << 4) >> 21);
+    t = r * 0x343FD + 0x269EC3;
+    t <<= 1;
+    RNG_0202f388 = t >> 1;
+    c.y = (p->s).coord.y + ((t << 3) >> 20) - 0x800;
+    CreateSmoke(3, &c);
+    (p->s).mode[1]++;
+  }
+  UpdateEntityAnim(&p->s);
+  if ((p->s).d.y <= 0x6FF) {
+    (p->s).d.y += 0x20;
+  }
+  (p->s).coord.y += (p->s).d.y;
+  (p->s).unk_coord.y = (p->s).coord.y;
+  if (Camera_GetDistance(&gStageRun.vm.camera, &(p->s).coord) > 0x3000) {
+    (p->s).flags &= ~DISPLAY;
+    SET_SOLID_ROUTINE(p, ENTITY_EXIT);
+  }
+}
 
 static const struct Collision sCollision = {
   kind : DRP,
