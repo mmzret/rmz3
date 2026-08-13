40307b699312995e191368bcd7ba43540c1ebfbe
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-12 21:17:36 -0400

    Match MenuExit_SeaOtterElf

--- src/cyberelf/sea_otter.c
+++ src/cyberelf/sea_otter.c
@@ -17,73 +17,37 @@ const ElfRoutine gSeaOtterElfRoutine = {
 };
 // clang-format on
 
-NAKED void MenuExit_SeaOtterElf(CyberElf* e) {
-  asm(".syntax unified\n\
-	push {lr}\n\
-	adds r3, r0, #0\n\
-	adds r0, #0xb4\n\
-	ldr r0, [r0]\n\
-	ldrb r2, [r3, #0x12]\n\
-	cmp r2, #0\n\
-	bne _080E455E\n\
-	adds r1, r0, #0\n\
-	adds r1, #0xb4\n\
-	ldrb r0, [r3, #0x13]\n\
-	ldrb r1, [r1]\n\
-	cmp r0, r1\n\
-	beq _080E45AE\n\
-	ldrb r1, [r3, #0xa]\n\
-	movs r0, #0xfe\n\
-	ands r0, r1\n\
-	b _080E4572\n\
-_080E455E:\n\
-	adds r1, r0, #0\n\
-	adds r1, #0xb4\n\
-	ldrb r0, [r3, #0x13]\n\
-	ldrb r1, [r1, #1]\n\
-	cmp r0, r1\n\
-	beq _080E45AE\n\
-	ldrb r1, [r3, #0xa]\n\
-	movs r0, #0xfe\n\
-	ands r0, r1\n\
-	movs r2, #0\n\
-_080E4572:\n\
-	movs r1, #0xfd\n\
-	ands r0, r1\n\
-	strb r0, [r3, #0xa]\n\
-	adds r0, r3, #0\n\
-	adds r0, #0x8c\n\
-	str r2, [r0]\n\
-	adds r0, #4\n\
-	str r2, [r0]\n\
-	adds r0, #4\n\
-	strb r2, [r0]\n\
-	ldrb r1, [r3, #0xa]\n\
-	movs r0, #0xfb\n\
-	ands r0, r1\n\
-	strb r0, [r3, #0xa]\n\
-	ldr r1, _080E45B4 @ =gElfFnTable\n\
-	ldrb r0, [r3, #9]\n\
-	lsls r0, r0, #2\n\
-	adds r0, r0, r1\n\
-	movs r1, #3\n\
-	str r1, [r3, #0xc]\n\
-	ldr r0, [r0]\n\
-	ldr r0, [r0, #0xc]\n\
-	str r0, [r3, #0x14]\n\
-	ldr r2, [r3, #0x2c]\n\
-	cmp r2, #0\n\
-	beq _080E45AE\n\
-	ldrb r1, [r2, #0xb]\n\
-	movs r0, #0x7f\n\
-	ands r0, r1\n\
-	strb r0, [r2, #0xb]\n\
-_080E45AE:\n\
-	pop {r0}\n\
-	bx r0\n\
-	.align 2, 0\n\
-_080E45B4: .4byte gElfFnTable\n\
- .syntax divided\n");
+void MenuExit_SeaOtterElf(CyberElf* e) {
+  CyberElf* linked = *(CyberElf**)e->buffer;
+  register u32 z asm("r2");
+  register u8 fl asm("r0");
+  register u8 fv asm("r1");
+  z = e->work[2];
+  if (z == 0) {
+    if (e->work[3] == linked->buffer[0]) {
+      return;
+    }
+    fv = e->flags;
+    fl = ~DISPLAY & fv;
+  } else {
+    u8* b = linked->buffer;
+    if (e->work[3] == b[1]) {
+      return;
+    }
+    fv = e->flags;
+    fl = ~DISPLAY & fv;
+    z = 0;
+  }
+  fl &= ~FLIPABLE;
+  e->flags = fl;
+  (e->body).status = z;
+  (e->body).prevStatus = z;
+  (e->body).invincibleTime = z;
+  e->flags &= ~COLLIDABLE;
+  SET_ELF_ROUTINE(e, ENTITY_DISAPPEAR);
+  if (e->unk_2c != NULL) {
+    (e->unk_2c)->flags2 &= 0x7f;
+  }
 }
 
 INCASM("asm/cyberelf/sea_otter.inc");
